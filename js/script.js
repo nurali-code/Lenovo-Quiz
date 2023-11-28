df54144d22b35461aa375f601ec3ff3ccddbd012
Nurali Davlatov <nur3.dav.97@gmail.com>
2023-11-29 00:48:48 +0500

    checker

--- js/script.js
+++ js/script.js
@@ -4,10 +4,45 @@ $(document).ready(function () {
         $(`.${targetClass}`).val($(this).val());
     });
 
+    function addError(parent, elem, mess) {
+        parent.find('.error').removeClass("error");
+        parent.find(".error__text").remove();
+        elem.addClass("error");
+        elem.after("<span class='error__text'>" + mess + "</span>");
+    }
+
     $('#first_form').on('submit', function (e) {
-        e.preventDefault(); // вызываем метод preventDefault(), чтобы остановить отправку формы
-        nextTab($(this).parents('.quiz-tab'));
+        e.preventDefault();
+        $(this).find('.btn').attr('disabled', 'disabled')
+
+        const formParet = $(this).parents('.quiz-tab');
+        const formEmail = $(this).find('input[name="u-email"]');
+        const formCompany = $(this).find('input[name="u-company"]');
+
+        var formData = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'sender.php',
+            data: formData,
+            success: function (response) {
+
+                $(formParet).find('.btn').removeAttr('disabled');
+                if (response === "company_false ") {
+                    addError(formParet, formCompany, 'Превышен лимит участников из вашей компании');
+                } else if (response === "email_false") {
+                    addError(formParet, formEmail, 'Почта уже существует');
+                } else if (response === "company_false email_false") {
+                    addError(formParet, formEmail, 'Почта уже существует');
+                    addError(formParet, formCompany, 'Превышен лимит участников из вашей компании');
+                } else {
+                    nextTab(formParet);
+                }
+
+            },
+            error: function (error) { console.error('Произошла ошибка:', error); }
+        });
     });
+
 
     const answers = { q1: 2, q2: 3, q3: 1, q4: 4, q5: 1, q6: 2, q7: 5, q8: 4, q9: 4, q10: 2, q11: 2, q12: 2, q13: 1, q14: 2, q15: 3, q16: 2, q17: 1, q18: 2, q19: 2 };
 
@@ -47,6 +82,9 @@ $(document).ready(function () {
             $('.quiz-resoult__text').html('Вы прошли викторину, и набрали <span>' + percentage + ' балов </span>')
             $('.quiz-resoult .quiz-form').show()
         }
+        if (percentage < 30) { $('.quiz-range__item').css('background-color', 'red') }
+        else if (percentage < 95) { $('.quiz-range__item').css('background-color', '#ffcd29b3') }
+        else { $('.quiz-range__item').css('background-color', '#6fbe47') }
         setTimeout(() => { $('.quiz-range__item').css('width', percentage + '%'); }, 1500);
     });
 
---
+++ sender.php
@@ -0,0 +1,56 @@
+<?php
+// Получение данных из формы
+$company = $_POST['u-company'];
+$email = $_POST['u-email'];
+
+// Загрузка содержимого JSON-файла
+$jsonData = file_get_contents('data.json');
+
+// Преобразование JSON в массив
+$dataArray = json_decode($jsonData, true);
+
+// Проверка наличия данных
+if ($dataArray !== null) {
+    $companyExists = false;
+    $emailExists = false;
+
+    foreach ($dataArray as $item) {
+        // Проверка существования компании
+        if ($item['u-company'] === $company) {
+            $companyExists = true;
+        }
+
+        // Проверка существования почты
+        if ($item['u-email'] === $email) {
+            $emailExists = true;
+        }
+
+        // Прерываем цикл, если оба условия уже выполнены
+        if ($companyExists && $emailExists) {
+            break;
+        }
+    }
+
+    // Проверка количества компаний
+    $countCompany = 0;
+    foreach ($dataArray as $item) {
+        if ($item['u-company'] === $company) {
+            $countCompany++;
+        }
+    }
+
+    // Вывод результата
+    if ($emailExists) {
+        echo "email_false";
+    } else {
+        if ($companyExists) {
+            if ($countCompany == 3) {
+                echo "company_false ";
+            }
+        }
+    }
+
+} else {
+    // echo "Ошибка при чтении JSON-файла.";
+}
+?>
